32b363e1ea6092dada9ee19733226b34a2fb8cc1
安田元斗 <yasudamototo@MacBook.cntrl1.zaq.ne.jp>
2019-09-16 16:12:00 +0900

    installed Elemnt UI and TypeScript

--- rails_app/app/javascript/app.vue
+++ rails_app/app/javascript/app.vue
@@ -7,15 +7,24 @@ p {
 
 <template>
   <div id="app">
-    <p>{{ message }}</p>
+    <el-table :data="message" size="mini" max-height="700" border style="width:100%">
+      <el-table-column prop="hello" label="Hello" sortable></el-table-column>
+    </el-table>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import { Table, TableColumn } from 'element-ui'
+
 export default {
+  components: {
+    'el-table': Table,
+    'el-table-column': TableColumn,
+  },
+
   data: function () {
     return {
-      message: "Hello Vue!"
+      message: [{hello: "Hello Vue!"}]
     }
   }
 }
