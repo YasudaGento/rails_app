440eba26471ee442982546613cfa8e86e86e769c
安田元斗 <yasudamototo@MacBook.cntrl1.zaq.ne.jp>
2020-02-15 17:02:44 +0900

    checked ajax function between front and end yarn install qs to make paramater

--- rails_app/app/controllers/demo_lists_controller.rb
+++ rails_app/app/controllers/demo_lists_controller.rb
@@ -4,6 +4,9 @@ class DemoListsController < ApplicationController
   end
 
   def get 
-
+    puts "--------------------------"
+    puts params.inspect
+    puts "--------------------------"
+    render json: {}, status: 200
   end
 end
--- rails_app/app/javascript/packs/demo_list.ts
+++ rails_app/app/javascript/packs/demo_list.ts
@@ -2,6 +2,8 @@ import Vue from "vue/dist/vue.esm";
 import lang from 'element-ui/lib/locale/lang/ja'
 import locale from 'element-ui/lib/locale'
 import List from "../components/demo_list/list.vue";
+import axios from 'axios'
+import Qs from "qs";
 
 locale.use(lang)
 
@@ -14,13 +16,28 @@ new Vue({
 
   data: {
     info: <Object[]>[{name: "yasuda"}],
+    q: <Object>{},
+    limit: <Number>0,
+    offset: <Number>0
   },
 
-  created: function(): void { this.get_info() },
+  created: function(): void { 
+    const url = '/demo_lists/get'
+    const params = this.qs({
+        q: this.query,
+        offset: this.offset,
+        limit: this.limit
+    })
+    axios.get(url, params,)
+         .then((res) => { console.log("yes") })
+   },
 
   methods: {
-    get_info: function(): void {
-      console.log(this.info)
-    }
+    qs: function(params){
+      return  {
+           params,
+           paramsSerializer: function (params) { return Qs.stringify(params, { arrayFormat: 'brackets' }); }
+       }
+    },
   }
 });
