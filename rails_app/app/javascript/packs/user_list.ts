35a0275e15cdfe08ecf91046e7839cdd51d656d2
安田元斗 <yasudamototo@MacBook.cntrl1.zaq.ne.jp>
2020-03-15 15:39:13 +0900

    add loading to user list page

--- rails_app/app/javascript/packs/user_list.ts
+++ rails_app/app/javascript/packs/user_list.ts
@@ -1,11 +1,12 @@
 import Vue from "vue/dist/vue.esm";
-import lang from 'element-ui/lib/locale/lang/ja'
-import locale from 'element-ui/lib/locale'
+import lang from 'element-ui/lib/locale/lang/ja';
+import locale from 'element-ui/lib/locale';
 import List from "../components/user/list.vue";
 import Search from "../components/user/search.vue";
-import http from '../libs/http.ts'
-import param_maker from '../libs/param_maker.ts'
-import error_notification from '../libs/error_notification.ts'
+import http from '../libs/http.ts';
+import param_maker from '../libs/param_maker.ts';
+import error_notification from '../libs/error_notification.ts';
+import { Loading } from "element-ui";
 
 locale.use(lang)
 type Parameter = { [s: string]: any };
@@ -31,7 +32,8 @@ new Vue({
   },
 
   methods: {
-    fetchList: function(){
+    fetchList: function(): void{
+      let loading = Loading.service({ text: "Loading..." });
       const url = '/users/get'
       const params = param_maker.get({
         offset: this.offset,
@@ -41,14 +43,15 @@ new Vue({
       http.fetch(url, params,)
           .then(this.successFetch)
           .catch(this.errorFetch)
+          .finally(loading.close());
     },
 
-    successFetch: function(res: Parameter){
+    successFetch: function(res: Parameter): void{
       this.info = res.data.info;
       this.mounted = true
     },
 
-    errorFetch: function(err: Parameter){
+    errorFetch: function(err: Parameter): void{
       error_notification.to_slack({
         file_name: "contact.js",
         function_name: "onRead()",
